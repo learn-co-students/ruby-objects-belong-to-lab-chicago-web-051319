4ea371b54e0abbd13a7ce7e08c4c5c537da2800c
afhare <alexandra.f.hare@gmail.com>
2019-05-16 14:02:26 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post   
+    attr_accessor :title, :author
+
+    # def author=(author)
+    #     @author = Author.name
+    # end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+    
+    attr_accessor :title, :artist
+
+    # def artist=(name)
+    #     name = Artist.name
+    # end
+end
